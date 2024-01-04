b77aeabc3a032c1475ec6815925a067a3baf8c66
Josh <76125168+joshyan1@users.noreply.github.com>
2024-01-03 21:46:28 -0500

    update

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import subprocess
 import discord
 from discord.ext import commands
 from discord import Intents
+import requests 
+import base64
+
 
 token = "token"
 intents = Intents.default()
@@ -14,20 +17,26 @@ bot = commands.Bot(command_prefix='!', intents = intents)
 my_dictionary =   {
     "model":"mistral",
     "lang":"english",
+    "ctx":[],
 }
+my_ollama = ollama.AsyncClient()
 
 #commands
 #change model, change landuage, start new convo,
 @bot.command()
-async def model(ctx, model):
+async def pull(ctx, model):
     print("ran")
     reply = await ctx.channel.send(f'Pulling {model} from Ollama...')
-    if ollama.pull(model)['status'] == "success":
-        my_dictionary["model"] = model
-        await bot.change_presence(activity=discord.Game(my_dictionary["model"]))
-        await reply.edit(content = f'Now using {model}')
-    else:
+    try:
+        pull = await my_ollama.pull(model)
+        print(pull)
+        if pull['status'] == "success":
+            my_dictionary["model"] = model
+            await bot.change_presence(activity = discord.Game(my_dictionary["model"]))
+            await reply.edit(content = f'Now using {model}')
+    except:
         await reply.edit(content = f"Sorry, we don't have {model} yet")
+        await model(ctx)
 
 @bot.command()
 async def lang(ctx, lang):
@@ -35,6 +44,27 @@ async def lang(ctx, lang):
     reply = await ctx.channel.send(f'Learning to speak {lang}')
     my_dictionary["lang"] = lang
 
+@bot.command()
+async def default(ctx):
+    print("resetting to defaults")
+    reply = await ctx.channel.send(f'Back to default settings.')
+    my_dictionary["model"] = "mistral"
+    my_dictionary["lang"] = "english"
+    my_dictionary["ctx"] = []
+
+@bot.command()
+async def newconvo(ctx):
+    print("reset conversation")
+    my_dictionary["ctx"] = []
+
+@bot.command()
+async def model(ctx):
+    reply = await ctx.channel.send(f'Find available models to run here: https://ollama.ai/library')
+
+#DEBUG
+@bot.command()
+async def ctx(ctx):
+    print(my_dictionary["ctx"])
 
 @bot.event
 async def on_ready():
@@ -44,28 +74,54 @@ async def on_ready():
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
-    context = []
-    output = "Generating response..."
+    print(message.attachments)
+    context = my_dictionary["ctx"]
+    output = ""
     #while True:
        #prompt = input("What do you wanna ask mistral? ")
     if message.author == bot.user:
         return
    
     if bot.user.mentioned_in(message):
+        #print(message.content)
+        message.content = message.content.replace("<@1188388112989175869>", "")
+        #print(message.content)
+
+        images = []
+        for i in message.attachments:
+            images.append(encode(i.url))
+        if (images == [] or my_dictionary["model"] != "llava") and message.content == "" :
+            return
+
+        task = asyncio.create_task(thinking(message))
         buffered = ''
-        reply = await message.channel.send(output)
+        # reply = await message.channel.send(output)
         output = ""
-        await bot.change_presence(activity=discord.CustomActivity("Thinking..."))
-        for i  in ollama.generate(my_dictionary["model"], system=f'respond in {my_dictionary["lang"]}', 
+        first_message = True
+        # async with message.channel.typing():
+        await bot.change_presence(activity=discord.CustomActivity("🤔"))
+        if True:
+        #if(images != [] and my_dictionary["model"] == "fafafafa"):
+            await llava(message, context, images, task, output, buffered)
+            return
+        
+        async for i in await my_ollama.generate(my_dictionary["model"], system=f'respond in {my_dictionary["lang"]}', 
                                   prompt=message.content, stream=True, context=context):
+            #print(i)
             if i["done"] == True:
-                context = i["context"]
+                my_dictionary["ctx"] = i["context"]
             else:
                 print(i["response"], end="", flush=True)
                 output += i["response"]
                 buffered += i['response']
-            if buffered[len(buffered)-1:len(buffered)] in ['.', '?', '!', ',' ':', '！', '。', '，', '\n'] and len(buffered) > 1:
-                await reply.edit(content=output)
+            #if buffered[len(buffered)-1:len(buffered)] in ['.', '?', '!', ',' ':', '！', '。', '，', '\n'] and len(buffered) > 1:
+            if len(buffered) > 37:
+                if first_message:
+                    reply = await message.channel.send(output)
+                    task.cancel()
+                    first_message = False
+                else:
+                    await reply.edit(content=output)
                 #subprocess.call(['say', '-v', 'Tingting', buffered])
                 buffered = ''
         if buffered:
@@ -74,7 +130,51 @@ async def on_message(message):
         print()
         await bot.change_presence(activity=discord.Game(my_dictionary["model"]))
         
-    
+async def llava(message, context, images, task, output, buffered):
+    first_message = True
 
+    if message.content == "":
+        message.content = "Analyze this image"
+    
+    async for i in await my_ollama.generate(my_dictionary["model"], prompt=message.content, stream=True, context=context, images=images):
+            #print(i)
+        if i["done"] == True:
+            my_dictionary["ctx"] = i["context"]
+        else:
+            print(i["response"], end="", flush=True)
+            output += i["response"]
+            buffered += i['response']
+        #if buffered[len(buffered)-1:len(buffered)] in ['.', '?', '!', ',' ':', '！', '。', '，', '\n'] and len(buffered) > 1:
+        if len(buffered) > 37:
+            if first_message:
+                reply = await message.channel.send(output)
+                task.cancel()
+                first_message = False
+            else:
+                await reply.edit(content=output)
+            #subprocess.call(['say', '-v', 'Tingting', buffered])
+            buffered = ''
+    if buffered:
+        await reply.edit(content=output)
+        #subprocess.call(['say', '-v', 'Tingting', buffered])
+    print()
+    await bot.change_presence(activity=discord.Game(my_dictionary["model"]))
+
+
+async def thinking(message):
+    timeout = 999
+    try:
+        await message.add_reaction('🤔')
+        async with message.channel.typing():
+            print("typing")
+            await asyncio.sleep(timeout)
+    except Exception as e:
+        print("failed")
+    finally:
+        await message.remove_reaction('🤔', bot.user)
+
+def encode(url):
+    return base64.b64encode(requests.get(url).content)
+    
 #client.run(token)
 bot.run(token)
